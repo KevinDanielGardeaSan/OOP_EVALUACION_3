ef815c7539401ce16fc1ab5d140b9e2c5cb6bd17
KevinDanielGardeaSan <158379717+KevinDanielGardeaSan@users.noreply.github.com>
2024-04-22 20:27:25 -0600

    Add files via upload

---
+++ EVA3_1_EXCEPCIONES/src/main/java/com/mycompany/eva3_1_excepciones/EVA3_1_EXCEPCIONES.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_excepciones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nacho
+ */
+public class EVA3_1_EXCEPCIONES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int num;
+        Scanner captura= new Scanner(System.in);
+        System.out.println("Introduce un valor entero: ");
+        num = captura.nextInt();
+        System.out.println("El valor es: "+ num);
+    }
+    
+}
