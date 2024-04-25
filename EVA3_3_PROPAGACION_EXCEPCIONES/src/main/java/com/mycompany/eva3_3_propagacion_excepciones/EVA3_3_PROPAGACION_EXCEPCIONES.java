2551b0139e451e69f72714bc5c721bac1bafa062
KevinDanielGardeaSan <158379717+KevinDanielGardeaSan@users.noreply.github.com>
2024-04-24 18:46:26 -0600

    Add files via upload

---
+++ EVA3_3_PROPAGACION_EXCEPCIONES/src/main/java/com/mycompany/eva3_3_propagacion_excepciones/EVA3_3_PROPAGACION_EXCEPCIONES.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_3_propagacion_excepciones;
+
+/**
+ *
+ * @author nacho
+ */
+public class EVA3_3_PROPAGACION_EXCEPCIONES {
+
+    public static void main(String[] args) {
+      A();
+    }
+    public static void A(){
+    B();
+    }
+    public static void B(){
+    C();
+    }
+    public static void C(){
+    int x = 10, y=0;
+    int resu = x / y;
+    System.out.println(resu);
+    }
+}
+    
